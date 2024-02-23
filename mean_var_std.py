563bccead1092b705458c3ff89aa5d89e5e25c9b
cheachu <cheachushenoy@gmail.com>
2024-02-23 17:18:40 +0530

    calculator

---
+++ mean_var_std.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def calculate(numbers):
+    # Convert the input list into a 3x3 numpy array
+    if len(numbers)!= 9:
+        raise ValueError('List must contain nine numbers.')
+
+    matrix = np.array(numbers).reshape(3, 3)
+
+    # Calculate statistics
+    mean = [list(np.mean(matrix, axis=0)), list(np.mean(matrix, axis=1)), np.mean(matrix)]
+    variance = [list(np.var(matrix, axis=0)), list(np.var(matrix, axis=1)), np.var(matrix)]
+    std_dev = [list(np.std(matrix, axis=0)), list(np.std(matrix, axis=1)), np.std(matrix)]
+    max_val = [list(np.max(matrix, axis=0)), list(np.max(matrix, axis=1)), np.max(matrix)]
+    min_val = [list(np.min(matrix, axis=0)), list(np.min(matrix, axis=1)), np.min(matrix)]
+    sum_val = [list(np.sum(matrix, axis=0)), list(np.sum(matrix, axis=1)), np.sum(matrix)]
+
+    # Create and return the dictionary
+    result_dict = {
+        'mean': mean,
+        'variance': variance,
+        'standard deviation': std_dev,
+        'max': max_val,
+        'min': min_val,
+        'sum': sum_val
+    }
+
+    return result_dict
